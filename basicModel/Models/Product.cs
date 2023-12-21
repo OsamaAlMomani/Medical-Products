a84a976efa19f82ebed939fd8778335a01c851dd
Okay Okay :) <osamamomani9560@outlook.com>
2023-12-21 11:22:08 +0300

    NEW INIT

---
+++ basicModel/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace basicModel.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Brand brand { get; set; }
+        public Product_Type product_type { get; set; }
+        public float price { get; set; }
+    }
+}
---
+++ basicModel/Models/Product_Type.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace basicModel.Models
+{
+    public class Product_Type
+    {
+        public Guid Id { get; set; }
+
+        public Category? category;
+        public int number { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
+
+
+
+/*
+ 
+    Category -> type -> Product
+         
+    
+ 
+*/
